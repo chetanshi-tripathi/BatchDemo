60ebca1f96c831bc72ce6e5edf7ed5fad089782c
chetanshi-tripathi <chetanshi.tripathi03@gmail.com>
2024-01-31 08:57:17 +0530

    added two batch projects with parent pom.xml

---
+++ demoBatchOne/src/main/java/com/example/demoBatchOne/DemoBatchOneApplication.java
@@ -0,0 +1,9 @@
+package com.example.demoBatchOne;
+
+public class DemoBatchOneApplication {
+
+	public static void main(String[] args) {
+		System.out.println("BATCH JOB - 1");
+	} 
+
+}
---
+++ demoBatchTwo/src/main/java/com/example/demoBatchTwo/DemoBatchTwoApplication.java
@@ -0,0 +1,9 @@
+package com.example.demoBatchTwo;
+
+public class DemoBatchTwoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("BATCH JOB -2");
+	}
+
+}
